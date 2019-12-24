d076496d62cf1113e341300ca6a3caa05a020d1d
Dinh Hai <dinhhhaii@gmail.com>
2019-12-24 09:53:50 +0700

    update routes

---
+++ src/routes-child.js
@@ -0,0 +1,12 @@
+import Maps from "views/Maps.jsx";
+
+const routes = [ 
+    {
+        path: '/test',
+        name: "Test",
+        exact: false,
+        component: Maps
+    }
+]
+
+export default routes;
